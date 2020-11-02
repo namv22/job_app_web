d97207a550007415a8c87fdfe4f6725c55a108f0
Unknown <namv221296@gmail.com>
2020-11-02 12:15:23 +0700

    Update list job and add job

--- src/components/AddJob.vue
+++ src/components/AddJob.vue
@@ -1,110 +1,134 @@
 <template>
-<div id="app">
+  <div id="app">
     <b-card border-variant="success" header="Add Post" class="card-add">
-        <label for="input-live">Name:</label>
-        <b-form-textarea id="textarea-default" size="20" v-model="newName" :state="nameState" aria-describedby="input-live-help input-live-feedback" placeholder="Enter post name" trim></b-form-textarea>
+      <label for="input-live">Job ID</label>
+      <b-form-input
+        id="input-live"
+        size="20"
+        v-model="newJobid"
+        aria-describedby="input-live-help input-live-feedback"
+        trim
+      ></b-form-input>
 
-        <!-- This will only be shown if the preceding input has an invalid state -->
-        <b-form-invalid-feedback id="input-live-feedback">
-            Enter name for post
-        </b-form-invalid-feedback>
+      <label for="input-live">Title</label>
+      <b-form-input
+        id="input-live"
+        size="20"
+        v-model="newTitle"
+        aria-describedby="input-live-help input-live-feedback"
+      ></b-form-input>
 
-        <label for="input-live">Youtube ID:</label>
-        <b-form-input id="input-live" size="20" v-model="newYT" :state="ytState" aria-describedby="input-live-help input-live-feedback" placeholder="Enter post Youtube ID" trim></b-form-input>
+      <label for="input-live">Company</label>
+      <b-form-input
+        id="input-live"
+        size="20"
+        v-model="newCompany"
+        aria-describedby="input-live-help input-live-feedback"
+      ></b-form-input>
 
-        <!-- This will only be shown if the preceding input has an invalid state -->
-        <b-form-invalid-feedback id="input-live-feedback">
-            Enter Youtube ID for post
-        </b-form-invalid-feedback>
+      <label for="input-live">Description</label>
+      <b-form-textarea
+        id="textarea-default"
+        size="20"
+        v-model="newDescription"
+        aria-describedby="input-live-help input-live-feedback"
+      ></b-form-textarea>
 
-        <b-form-group label="Tag">
-            <b-form-radio v-model="newTag" name="some-radios" value="game">Game</b-form-radio>
-            <b-form-radio v-model="newTag" name="some-radios" value="music">Music</b-form-radio>
-            <b-form-radio v-model="newTag" name="some-radios" value="meme">Meme</b-form-radio>
-        </b-form-group>
-        <div class="mt-3">Selected: <strong>{{ newTag }}</strong></div>
+      <label for="input-live">Salary</label>
+      <b-form-input
+        id="input-live"
+        size="20"
+        type="number"
+        v-model="newSalary"
+        aria-describedby="input-live-help input-live-feedback"
+        trim
+      ></b-form-input>
 
-        <br />
-        <b-button variant="outline-success" @click="addPost">
-            Add Post
-        </b-button>
+      <br />
+      <b-button variant="outline-success" @click="addPost">
+        Add Post
+      </b-button>
     </b-card>
-    <br/>
-</div>
+    <br />
+  </div>
 </template>
 
 <script>
-import {
-    db
-} from '../firebase';
-import _ from 'lodash';
+import { db } from "../firebase";
+import _ from "lodash";
 
 export default {
-    name: 'TableCard',
-    computed: {
-        nameState() {
-            return this.newName.length > 0 ? true : false
-        },
-        ytState() {
-            return this.newYT.length > 0 ? true : false
-        },
-        orderedPosts: function () {
-            return _.orderBy(this.posts, ['date'], ['desc'])
-        },
+  name: "TableCard",
+  computed: {
+    nameState() {
+      return this.newName.length > 0 ? true : false;
     },
-    data() {
-        return {
-            posts: [],
-            newName: '',
-            newYT: '',
-            newTag: '',
-        }
+    ytState() {
+      return this.newYT.length > 0 ? true : false;
     },
-    firestore() {
-        return {
-            posts: db.collection('jobs'),
-        }
+    orderedPosts: function() {
+      return _.orderBy(this.posts, ["date"], ["desc"]);
     },
-    methods: {
-        addPost: function () {
-            this.$firestore.posts.add({
-                name: this.newName,
-                yturl: this.newYT,
-                tag: this.newTag,
-                date: new Date()
-            });
-            this.newName = '';
-            this.newYT = '';
-            this.newTag = '';
-        },
-        deletePost: function (post) {
-            this.$firestore.posts.doc(post['.key']).delete();
-        }
-    }
-}
+  },
+  data() {
+    return {
+      posts: [],
+      newJobid: "",
+      newTitle: "",
+      newCompany: "",
+      newDescription: "",
+      newSalary: "",
+    };
+  },
+  firestore() {
+    return {
+      posts: db.collection("jobs"),
+    };
+  },
+  methods: {
+    addPost: function() {
+      this.$firestore.posts.add({
+        id: this.newJobid,
+        title: this.newTitle,
+        company: this.newCompany,
+        description: this.newDescription,
+        salary: this.newSalary,
+        created: new Date(),
+      });
+      this.newJobid = "";
+      this.newTitle = "";
+      this.newCompany = "";
+      this.newDescription = "";
+      this.newSalary = "";
+    },
+    deletePost: function(post) {
+      this.$firestore.posts.doc(post[".key"]).delete();
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 
 .card-add {
-    width: 500px;
-    position: relative;
-    margin-left: auto;
-    margin-right: auto;
+  width: 500px;
+  position: relative;
+  margin-left: auto;
+  margin-right: auto;
 }
 
 .card-post {
-    width: 800px;
-    position: relative;
-    margin-left: 50%;
-    margin-right: 50%;
+  width: 800px;
+  position: relative;
+  margin-left: 50%;
+  margin-right: 50%;
 }
 </style>
--- src/components/ListJob.vue
+++ src/components/ListJob.vue
@@ -1,98 +1,180 @@
 <template>
-<div id="app">
-    <b-card border-variant="info" header="Posts" align="center" class="container-fluid md-elevation-12">
-        <md-table md-card class="container-fluid md-elevation-12">
+  <div id="app">
+    <b-card
+      border-variant="info"
+      header="Posts"
+      align="center"
+      class="container-fluid md-elevation-12"
+    >
+      <md-table md-card class="container-fluid md-elevation-12">
+        <md-table-row align="left">
+          <md-table-head>Company</md-table-head>
+          <md-table-head>Position</md-table-head>
+          <md-table-head>Description</md-table-head>
+          <md-table-head>Salary</md-table-head>
+        </md-table-row>
 
-            <md-table-row align="left">
-                <md-table-head>Company</md-table-head>
-                <md-table-head>Position</md-table-head>
-                <md-table-head>Description</md-table-head>
-                <md-table-head>Salary</md-table-head>
-            </md-table-row>
-
-            <md-table-row v-for="post in orderedPosts" v-bind:key="post.name" align="left">
-                <md-table-cell>{{ post.company }}</md-table-cell>
-                <md-table-cell>{{ post.title }}</md-table-cell>
-                <md-table-cell>{{ post.description }}</md-table-cell>
-                <md-table-cell>{{ post.salary }}</md-table-cell>
-            </md-table-row>
-        </md-table>
+        <md-table-row
+          v-for="post in orderedPosts"
+          v-bind:key="post.name"
+          align="left"
+        >
+          <md-table-cell>{{ post.company }}</md-table-cell>
+          <md-table-cell>{{ post.title }}</md-table-cell>
+          <md-table-cell>{{ post.description }}</md-table-cell>
+          <md-table-cell>{{ post.salary }}</md-table-cell>
+          <md-table-cell>
+            <b-button
+              variant="outline-info"
+              @click="sendInfo(post)"
+              v-b-modal.modal-xl="'job'"
+              >Information</b-button
+            >
+            &nbsp;
+            <b-button
+              variant="outline-primary"
+              @click="sendInfo(post)"
+              v-b-modal.modal-xl="'edit'"
+              >Edit</b-button
+            >
+            &nbsp;
+            <b-button variant="outline-danger" @click="deletePost(post)">
+              Remove
+            </b-button>
+          </md-table-cell>
+        </md-table-row>
+      </md-table>
     </b-card>
-</div>
+    <b-modal
+      size="xl"
+      id="job"
+      :title="selectedPost.company + ' - ' + selectedPost.title"
+    >
+      <p></p>
+    </b-modal>
+    <b-modal
+      size="xl"
+      id="edit"
+      :title="selectedPost.company + ' - ' + selectedPost.title"
+    >
+      <label for="input-live">Job ID</label>
+      <b-form-input
+        id="input-live"
+        size="20"
+        v-model="newJobid"
+        aria-describedby="input-live-help input-live-feedback"
+        trim
+        :value="selectedPost.id"
+      ></b-form-input>
+
+      <label for="input-live">Title</label>
+      <b-form-input
+        id="input-live"
+        size="20"
+        v-model="newTitle"
+        aria-describedby="input-live-help input-live-feedback"
+        :value="selectedPost.title"
+      ></b-form-input>
+
+      <label for="input-live">Company</label>
+      <b-form-input
+        id="input-live"
+        size="20"
+        v-model="newCompany"
+        aria-describedby="input-live-help input-live-feedback"
+        :value="selectedPost.company"
+      ></b-form-input>
+
+      <label for="input-live">Description</label>
+      <b-form-textarea
+        id="textarea-default"
+        size="20"
+        v-model="newDescription"
+        aria-describedby="input-live-help input-live-feedback"
+        :value="selectedPost.description"
+      ></b-form-textarea>
+
+      <label for="input-live">Salary</label>
+      <b-form-input
+        id="input-live"
+        size="20"
+        type="number"
+        v-model="newSalary"
+        aria-describedby="input-live-help input-live-feedback"
+        trim
+        :value="selectedPost.salary"
+      ></b-form-input>
+
+      <br />
+      <b-button variant="outline-success">
+        Edit Post
+      </b-button>
+    </b-modal>
+  </div>
 </template>
 
 <script>
-import {
-    db
-} from '../firebase';
-import _ from 'lodash';
+import { db } from "../firebase";
+import _ from "lodash";
 
 export default {
-    name: 'TableCard',
-    computed: {
-        nameState() {
-            return this.newName.length > 0 ? true : false
-        },
-        ytState() {
-            return this.newYT.length > 0 ? true : false
-        },
-        orderedPosts: function () {
-            return _.orderBy(this.posts, ['date'], ['desc'])
-        },
+  name: "TableCard",
+  computed: {
+    orderedPosts: function() {
+      return _.orderBy(this.posts, ["created"], ["desc"]);
     },
-    data() {
-        return {
-            posts: [],
-            newName: '',
-            newYT: '',
-            newTag: '',
-        }
+  },
+  data() {
+    return {
+      posts: [],
+      selectedPost: "",
+      newJobid: "",
+      newTitle: "",
+      newCompany: "",
+      newDescription: "",
+      newSalary: "",
+    };
+  },
+  firestore() {
+    return {
+      posts: db.collection("jobs"),
+    };
+  },
+  methods: {
+    deletePost: function(post) {
+      this.$firestore.posts.doc(post[".key"]).delete();
     },
-    firestore() {
-        return {
-            posts: db.collection('jobs'),
-        }
+    // editPost: function(post) {
+
+    // },
+    sendInfo(post) {
+      this.selectedPost = post;
     },
-    methods: {
-        addPost: function () {
-            this.$firestore.posts.add({
-                name: this.newName,
-                yturl: this.newYT,
-                tag: this.newTag,
-                date: new Date()
-            });
-            this.newName = '';
-            this.newYT = '';
-            this.newTag = '';
-        },
-        deletePost: function (post) {
-            this.$firestore.posts.doc(post['.key']).delete();
-        }
-    }
-}
+  },
+};
 </script>
 
 <style>
 #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 
 .card-add {
-    width: 500px;
-    position: relative;
-    margin-left: auto;
-    margin-right: auto;
+  width: 500px;
+  position: relative;
+  margin-left: auto;
+  margin-right: auto;
 }
 
 .card-post {
-    width: 800px;
-    position: relative;
-    margin-left: 50%;
-    margin-right: 50%;
+  width: 800px;
+  position: relative;
+  margin-left: 50%;
+  margin-right: 50%;
 }
 </style>
